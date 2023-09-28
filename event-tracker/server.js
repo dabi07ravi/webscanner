a570e44cbf1eaa4b58b415916a021cabd1294f82
Arcgate-pradhyuman <pradhyuman@arcgate.com>
2023-09-28 17:47:18 +0530

    ejs form for data insertion

--- event-tracker/server.js
+++ event-tracker/server.js
@@ -2,8 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const dbConfig = require("./config/database"); // Import database configuration
-const dbConnection = require('./utils/database.utils')
-
+const path = require('path');
 //ravi changes
 const eventRouter = require("./routes/event.router");
 
@@ -19,6 +18,14 @@ app.use(express.json());
 //api
 //ravi changes
 app.use("/api/event", eventRouter);
+
+// Setting up EJS as view engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
 
 // Starting the server
 app.listen(PORT, () => {
