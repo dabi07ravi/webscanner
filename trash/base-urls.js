2f4a7d8f3e579ba9c93a1694f48a5debaebe17cb
Arcgate-pradhyuman <pradhyuman@arcgate.com>
2023-09-29 11:01:48 +0530

    removed trash folder

--- trash/base-urls.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-    "https://events.clarionevents.com/hvi2023/Public/eventmap.aspx?shMode=E&ID=80382"
-    // "https://www.npmjs.com/package/benchmark"
-]
-
-const year = 2023
-`https://events.clarionevents.com/hvi${year}/Public/eventmap.aspx?shMode=E&ID=80382`
